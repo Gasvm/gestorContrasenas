e38f85b83aba80db8412dafc2d1fc042d7b26f41
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-11-17 19:52:04 +0100

    Test en js

--- www/scripts.js
+++ www/scripts.js
@@ -1,3 +1,19 @@
+async function getCategories() {
+    try {
+        const response = await fetch('http://localhost:3000/categories');
+        if (response.ok) {
+            const data = await response.json(); 
+            console.log(data);
+        } else {
+            throw new Error('Failed to fetch data');
+        }
+    } catch (error) {
+        console.error('Error:', error); 
+    }
+}
+
+
+
 let drawData = (data) => {
     data.forEach(category => {
       let parent = document.getElementsByTagName('ul')[0]
